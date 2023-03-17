71e6f9226810c5ae4ca36f9f00e224582ee83446
Zachary Boelter <zboelter@gmail.com>
2023-03-17 11:07:28 -0700

    set variables, print sum and remainder

--- index.py
+++ index.py
@@ -2,7 +2,10 @@ from datetime import date
 
 
 # import datetime from date, set it to var
-
 todays_date = date.today()
-
 print(todays_date)
+
+# set vars, print sum and remainder
+product = 2 * 2
+remainder = 1398 % 11
+print(product, remainder)

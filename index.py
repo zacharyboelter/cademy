bed2db646a9e049e92315b1d7b15e3e2573741aa
Zachary Boelter <zboelter@gmail.com>
2023-03-17 11:16:59 -0700

    set vars, set cost, print type and total cost

--- index.py
+++ index.py
@@ -9,3 +9,13 @@ print(todays_date)
 product = 2 * 2
 remainder = 1398 % 11
 print(product, remainder)
+
+
+# set cucumber vars, set cost, print type and total cost
+cucumbers = 1
+price_per_cucumber = 3.25
+
+total_cost = cucumbers * price_per_cucumber
+
+print(type(total_cost))
+print(total_cost)

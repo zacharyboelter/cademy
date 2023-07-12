391770ad0b22e150d07f24141cd6560e7c336c57
Zachary Boelter <zboelter@gmail.com>
2023-07-12 12:27:54 -0700

    jaden case problem set codewars

--- index.py
+++ index.py
@@ -20,3 +20,9 @@ total_cost = cucumbers * price_per_cucumber
 print(type(total_cost))
 print(total_cost)
 
+# jaden case, basically just capitalize the first letter of every word in a given string
+
+def to_jaden_case(string):
+    return ' '.join(word.capitalize() for word in string.split())
+
+print(to_jaden_case("hello, how's it going t'day?"))
